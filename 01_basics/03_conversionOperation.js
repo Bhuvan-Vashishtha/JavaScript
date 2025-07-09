14c5c124025622543210160fd8c6338119f946f1
Bhuvan-Vashishtha <bhuvanvashishtha656@gmail.com>
2025-07-09 05:52:17 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+let someNumber = 33
+let stringNumber = String(someNumber)
