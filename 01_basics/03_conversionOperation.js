e8920342d0335ed17667ad3e5c46877aee3fe9c0
Bhuvan-Vashishtha <bhuvanvashishtha656@gmail.com>
2025-07-09 06:16:42 +0000

    14c5c124025622543210160fd8c6338119f946f1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,41 @@
 let score = "33abc"
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-let someNumber = 33
-let stringNumber = String(someNumber)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1+str2
+console.log(str3)
+console.log("1" + 2)
+console.log(1 + "2")
+
+//console.log(3 + 4 * 5 % 3)
+
+console.log(+true);
+console.log(+"")
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+
+console.log(++gameCounter)
