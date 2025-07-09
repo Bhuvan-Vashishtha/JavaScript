5f71dcfae9a4433eb9e643409b17bac08d8423ed
Bhuvan-Vashishtha <bhuvanvashishtha656@gmail.com>
2025-07-09 05:51:07 +0000

    Conversion of datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number
+// big int
+// string
+// boolean
+// null 
+// undefined
+//symbol
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
