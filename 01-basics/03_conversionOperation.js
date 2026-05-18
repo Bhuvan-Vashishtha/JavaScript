5b5011432ee128867d78214ef0d884aa28db1a47
kocho <bhuvanvashishtha656@gmail.com>
2026-05-18 16:40:43 +0000

    datatypes_conversion

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null -> 0 in a number
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "hitesh" => true
+
