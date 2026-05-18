4a529043851967393fae5069503804f92ad12f50
kocho <bhuvanvashishtha656@gmail.com>
2026-05-18 16:19:20 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use Strict"; // treat all JS code as newer version
+
+//code readability should be high
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//primitive data types
+
+//number => 2 to power 53
+//bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value /// it is an object
+//undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);
